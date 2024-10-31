42d4273d825a7eba47fbda94bf29998c75718afc
tom <141278466+tomlukewarden@users.noreply.github.com>
2024-10-31 14:03:57 +0000

    starting on shift

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<main class="container my-5">
+  <div class="row align-items-center">
+    <div class="col-12 col-md-6 mb-4 mb-md-0">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/bridge.jpg" alt="header image" class="img-fluid rounded">
+    </div>
+    <div class="col-12 col-md-6">
+      <h3>Welcome to Culley & McAlpine</h3>
+      <p>We are one of the leading Solicitors firms based in Perth, Scotland.</p>
+      <p>The Firm was founded in 2000, and our current team of solicitors has over 100 years of experience.</p>
+      <p>Much of our work comes from referrals and repeat clients. We pride ourselves on delivering the highest
+        standard of care and tailoring our services to best meet your needs.</p>
+    </div>
+  </div>
+</main>
+
+<section class="bg-light py-5">
+  <div class="container">
+    <h3 class="text-center mb-4">Here's how to get in touch</h3>
+    <div class="row text-center g-4">
+      <!-- Opening Times, Contact Info, Address -->
+    </div>
+  </div>
+</section>
+
+<section class="py-5">
+  <div class="container">
+    <h3 class="text-center mb-4">How to find us</h3>
+    <div class="ratio ratio-16x9">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2208.2216793838434!2d-3.4283305999999896!3d56.39500100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48863b110be51be5%3A0x53a57647bc371ba3!2sCulley%20%26%20McAlpine!5e0!3m2!1sen!2suk!4v1724323271098!5m2!1sen!2suk"
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
