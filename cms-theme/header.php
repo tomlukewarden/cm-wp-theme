44cefecb5a18128efd4f71f3c153f91bf5f3d1f9
tom <141278466+tomlukewarden@users.noreply.github.com>
2025-01-09 10:36:04 +0000

    adding pages

---
+++ cms-theme/header.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
+<nav class="navbar navbar-expand-lg navbar-dark mb-5">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="home.html">
+        <img class="logo img-fluid" src="./assets/thumbnail_image001.png" alt="Culley & McAlpine Logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../about/about.html">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../contact/contact.html">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../meet-the-team/meet-the-team.html">Meet the Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../services/services.html">Services</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
